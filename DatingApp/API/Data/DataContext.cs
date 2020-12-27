6344b5282be052bb49db0328588e1507afba6d27
Micah <PocketSizedAnimations@gmail.com>
2020-12-27 07:38:56 -0500

    End of Section 5

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -7,7 +7,7 @@ namespace Api.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-
+            
         }
 
         public DbSet<AppUser> Users { get; set; }
